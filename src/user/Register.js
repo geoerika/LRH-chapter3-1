e22c2f04c92670ad19171d619824342fbab85640
Erika Szabo <erika.szabo.geo@gmail.com>
2020-04-24 18:24:04 -0400

    arranged html

--- src/user/Register.js
+++ src/user/Register.js
@@ -3,12 +3,18 @@ import React from 'react'
 export default function Register () {
   return (
     <form onSubmit={ e => e.preventDefault() }>
-      <label htmlFor='register-username'>Usrname:</label>
-      <input type='text' name='register-username' id='register-username' />
-      <label htmlFor='register-password'>Password:</label>
-      <input type='password' name='register-password' id='register-password' />
-      <label htmlFor="register-password-repeat">Repeat password:</label>
-      <input type="password" name="register-password-repeat" id="register-password-repeat" />
+      <div>
+        <label htmlFor='register-username'>Usrname:</label>
+        <input type='text' name='register-username' id='register-username' />
+      </div>
+      <div>
+        <label htmlFor='register-password'>Password:</label>
+        <input type='password' name='register-password' id='register-password' />
+      </div>
+      <div>
+        <label htmlFor="register-password-repeat">Repeat password:</label>
+        <input type="password" name="register-password-repeat" id="register-password-repeat" />
+      </div>
       <input type='submit' value='Register' />
     </form>
   )
