4c3473ba37147264b4692b3783d63d5b7f68762b
Erika Szabo <erika.szabo.geo@gmail.com>
2020-04-24 20:00:00 -0400

    created dynamic posts

--- src/post/CreatePost.js
+++ src/post/CreatePost.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function CreatePost ({ user }) {
+export default function CreatePost ({ user, posts, setPosts}) {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  const handleTitle = (evt) => {
+    setTitle(evt.target.value)
+  }
+
+  const handleContent = (evt) => {
+    setContent(evt.target.value)
+  }
+
+  const handleCreate = () => {
+    const newPost = {
+      title,
+      content,
+      author: user
+    }
+    setPosts([newPost, ...posts])
+  }
+
   return (
-    <form onSubmit={ e => e.preventDefault() } >
+    <form onSubmit={ e => {
+      e.preventDefault()
+      handleCreate()
+    }} >
       <div>Author: <b>{ user }</b></div>
       <div>
         <label htmlFor='create-title'>Title:</label>
-        <input type='text' name='create-title' id='create-title' />
+        <input type='text' value={ title  } onChange={ handleTitle } name='create-title' id='create-title' />
       </div>
       <div>
-        <textarea />
+        <textarea value={ content } onChange={ handleContent }/>
       </div>
       <input type='submit' value='Create' />
     </form>
