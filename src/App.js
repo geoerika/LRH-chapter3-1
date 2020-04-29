72c77557e7728c6c6e99623cdfae4a327b615e36
Erika Szabo <erika.szabo.geo@gmail.com>
2020-04-29 14:03:13 -0400

    used useEfect to change document title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import appReducer from './reducers'
 
 import PostList from './post/PostList'
@@ -19,6 +19,15 @@ export default function App () {
  
  const [state, dispatch] = useReducer(appReducer, { user: '', posts: defaultPosts })
  const { user, posts } = state
+
+ useEffect(() => {
+   if (user) {
+     document.title = `${user} - React Hooks Blog`
+   } else {
+     document.title = 'React Hooks Blog'
+   }
+ }, [user])
+ 
   return (
     <div style={{ padding: 8 }}>
       <UserBar user={ user } dispatch={ dispatch }/>
